29c42ab5b2e4d77a397298ef399d3a294218a436
Selman Orhan <selmanorhan123@gmail.com>
2024-05-25 23:11:12 +0300

    node modules and other fixes

---
+++ src/helpers/sync.js
@@ -0,0 +1,16 @@
+"use strict"
+/* -------------------------------------------------------
+------------------------------------------------------- */
+// sync():
+
+module.exports = async function() {
+
+    return null;
+
+    /* CLEAR DATABASE */
+    const { mongoose } = require('../configs/dbConnection')
+    await mongoose.connection.dropDatabase()
+    console.log('- Database and all data DELETED!')
+    /* CLEAR DATABASE */
+
+}
